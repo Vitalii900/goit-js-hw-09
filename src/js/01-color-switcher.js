a9cb92de4a527979a533a708704e24f519bd6fa7
Vitalii900 <melnikvitaliy30@gmail.com>
2022-12-04 09:31:07 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyRef = document.querySelector('body');
+
+const buttonStartRef = document.querySelector('button[data-start]');
+
+const buttonStopRef = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+buttonStartRef.addEventListener('click', startSwitcher);
+
+buttonStopRef.addEventListener('click', stopSwitcher);
+
+function startSwitcher() {
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    buttonStartRef.setAttribute('disabled', '');
+    buttonStopRef.removeAttribute('disabled');
+}
+
+function stopSwitcher() {
+    clearInterval(timerId);
+    buttonStopRef.setAttribute('disabled', '');
+    buttonStartRef.removeAttribute('disabled');
+}
